87aafb82bbfd7028d4c9720c0b23e620727bb333
vikas bajpayee <mainhujyan@gmail.com>
2017-06-30 05:40:09 +0530

    initial commit

---
+++ app/src/main/java/com/example/dell/pixectra/PhotoBook.java
@@ -0,0 +1,13 @@
+package com.example.dell.pixectra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_book);
+    }
+}
